3baa2e59e9a892580b0cf3ba4f3fe39c8e2754d7
TimirKundu <timirbarankundu@gmail.com>
2018-04-25 00:37:34 +0530

    Initial Commit

---
+++ src/app/courses.component.ts
@@ -0,0 +1,12 @@
+
+import { Component } from '@angular/core'
+import { CoursesService } from './courses.service';
+@Component({
+    selector: 'courses',
+    template: `
+            <button class="btn btn-primary" [class.active]="isActive">Save Me</button>
+            `
+})
+export class CoursesComponent {
+    isActive = false;
+}
