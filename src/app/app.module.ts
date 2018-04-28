18baf74e1f288cf30429e1ea319e0a25136f87f2
Timir Baran Kundu <timirbarankundu@Timirs-Mac-mini.local>
2018-04-28 20:52:59 +0530

    Bindings completed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { SummaryPipe } from './summary.pipe';
 import { CoursesService } from './courses.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms'
@@ -9,6 +10,8 @@ import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
 import { AuthorsComponent } from './authors/authors.component';
 import { AuthorsService } from './authors.service';
+import { FavouriteComponent } from './favourite/favourite.component';
+import { TitleCasePipe } from './title-case.pipe';
 
 
 @NgModule({
@@ -16,7 +19,11 @@ import { AuthorsService } from './authors.service';
     AppComponent,
     CoursesComponent,
     CourseComponent,
-    AuthorsComponent
+    AuthorsComponent,
+    SummaryPipe,
+    FavouriteComponent,
+    FavouriteComponent,
+    TitleCasePipe
   ],
   imports: [
     BrowserModule,
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,18 +4,9 @@ import { CoursesService } from './courses.service';
 @Component({
     selector: 'courses',
     template: `
-    {{ course.title | uppercase | lowercase }} <br/>
-    {{ course.students | number }} <br/>
-    {{ course.rating | number: '2.1-1' }} <br/>
-    {{ course.price | currency: 'INR' }} <br/>
-    {{ course.releaseDate | date: 'shortDate' }}`
+            {{ text | summary:10 }}
+    `
 })
 export class CoursesComponent {
-    course = {
-        title: "The complete angular course",
-        rating: 4.9745,
-        students: 30123,
-        price: 190.95,
-        releaseDate: new Date(2016,3,1)
-    }
+    text = 'Lorem Ipsum is simply dummy text of the printing'
 }
