8e87c077f30bfa38f85e48c07bfd0c4dbd6e2b75
Timir Baran Kundu <timirbarankundu@Timirs-Mac-mini.local>
2018-05-05 23:55:19 +0530

    view encapsulation

--- src/app/favourite/favourite.component.ts
+++ src/app/favourite/favourite.component.ts
@@ -1,19 +1,16 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
+//import { ViewEncapsulation } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'favourite',
   templateUrl: './favourite.component.html',
   styleUrls: ['./favourite.component.css'],
-  inputs: ['isFavorite']
+  encapsulation: ViewEncapsulation.Emulated  
 })
-export class FavouriteComponent implements OnInit {
+export class FavouriteComponent {
 
   @Input('isFavorite') isSelected: boolean;
   @Output('change') click = new EventEmitter();
-  constructor() { }
-
-  ngOnInit() {
-  }
 
   onClick() {
     this.isSelected = !this.isSelected;
